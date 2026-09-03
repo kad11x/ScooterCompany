21fb63d119e4456e0182a0b9d8e4f19b218bfeec
Your Name <you@example.com>
2026-09-03 09:33:57 +0200

    DbContext is contected and pushed, db: ScooterCompany is created and up and running

--- DataAccess/DataAccessContext.cs
+++ DataAccess/DataAccessContext.cs
@@ -2,14 +2,29 @@
 using System.Reflection.Emit;
 using static System.Collections.Specialized.BitVector32;
 using Microsoft.EntityFrameworkCore;
+using Core.Model;
 
 namespace DataAccess
 {
     public class DataAccessContext : DbContext
     {
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql("Host=localhost;Port=55432;Database=ScooterCompany;Username=postgres;Password=mypassword");
+        }
+
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        
+        public DbSet<Scooter> Scooters { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+
+
+       
+
     }
-    
+
 
 }
 
---
+++ DataAccess/Migrations/20260903073005_InitialCreate.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Scooters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Brand = table.Column<string>(type: "text", nullable: true),
+                    BatteryCapacity = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scooters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trips",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StartTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Distance = table.Column<double>(type: "double precision", nullable: false),
+                    Cost = table.Column<decimal>(type: "numeric", nullable: false),
+                    AppUserId = table.Column<int>(type: "integer", nullable: false),
+                    ScooterId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trips", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Trips_AppUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AppUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Trips_Scooters_ScooterId",
+                        column: x => x.ScooterId,
+                        principalTable: "Scooters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_AppUserId",
+                table: "Trips",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_ScooterId",
+                table: "Trips",
+                column: "ScooterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trips");
+
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+
+            migrationBuilder.DropTable(
+                name: "Scooters");
+        }
+    }
+}
